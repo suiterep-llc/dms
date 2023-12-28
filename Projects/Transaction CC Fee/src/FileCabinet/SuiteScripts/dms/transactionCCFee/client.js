087d6f648095bafdcd2e734da6995e5a5c6ccad5
Stephen Lemp <stephen@suiterep.com>
2023-12-28 09:56:06 -0500

    request confirmation of fee on save

--- Projects/Transaction CC Fee/src/FileCabinet/SuiteScripts/dms/transactionCCFee/client.js
+++ Projects/Transaction CC Fee/src/FileCabinet/SuiteScripts/dms/transactionCCFee/client.js
@@ -6,6 +6,7 @@ define(['N/query'], function (query) {
   * @module /SuiteScripts/dms/transactionCCFee/client.js
   */
   const TRANSACTION_FEE_MESSAGE = `Please note a 2.5% charge will be added on payment submission for all payments via Payment Card.`;
+  const CONFIRM_CC_FEE_MESSAGE = `Please confirm your agreement to be charged a 2.5% convenience fee for using a Payment Card. Click "Ok" to confirm or "Cancel" to go back and update payment method.`;
 
   const GET_IS_CC_QUERY = `
     select
@@ -24,7 +25,7 @@ define(['N/query'], function (query) {
     console.log('fieldChanged', { fieldId, ...context });
     if (fieldId == 'paymentoption') {
       const paymentOption = currentRecord.getValue({ fieldId });
-      const isPaymentCard = getisPaymentCard(paymentOption);
+      const isPaymentCard = getIsPaymentCard(paymentOption);
       console.log(`Payment Option (${paymentOption}) is credit card (${isPaymentCard})`);
       if (isPaymentCard) {
         alert(TRANSACTION_FEE_MESSAGE);
@@ -34,23 +35,25 @@ define(['N/query'], function (query) {
 
 
   function saveRecord(context) {
-    const paymentOption = currentRecord.getValue({ fieldId });
-    const isPaymentCard = getisPaymentCard(paymentOption);
+    const { currentRecord } = context;
+    const paymentOption = currentRecord.getValue({ fieldId: 'paymentoption' });
+    const isPaymentCard = getIsPaymentCard(paymentOption);
     if (!isPaymentCard) {
       return true;
+    } else {
+      return confirm(CONFIRM_CC_FEE_MESSAGE);
     }
-
-    return true;
   }
 
 
-  function getisPaymentCard(paymentOption) {
+  function getIsPaymentCard(paymentOption) {
     if (!paymentOption) return false;
 
     const queryResults = query.runSuiteQL({ query: GET_IS_CC_QUERY, params: [paymentOption] }).asMappedResults();
     if (queryResults.length != 1) return false;
     return queryResults[0].ispaymentcard == 'T';
   }
+
 
   return { saveRecord, fieldChanged };
 });
