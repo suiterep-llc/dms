bd85d882637a40a5da1d55c37a238c11027d9c40
Stephen Lemp <stephen@suiterep.com>
2024-01-09 09:54:17 -0500

    remove unused code

--- Projects/Transaction CC Fee/src/FileCabinet/SuiteScripts/dms/transactionCCFee/client.js
+++ Projects/Transaction CC Fee/src/FileCabinet/SuiteScripts/dms/transactionCCFee/client.js
@@ -6,22 +6,7 @@ define(['./commonLibrary'], function (library) {
   * @module /SuiteScripts/dms/transactionCCFee/client.js
   */
   const FEE_PERCENTAGE = 2.5;
-  const TRANSACTION_FEE_MESSAGE = `Please note a ${FEE_PERCENTAGE}% charge will be added on payment submission for all payments via Payment Card.`;
   const CONFIRM_CC_FEE_MESSAGE = `Please confirm your agreement to be charged a ${FEE_PERCENTAGE}% convenience fee for using a Payment Card. Click "Ok" to confirm or "Cancel" to go back and update payment method.`;
-
-
-  function fieldChanged(context) {
-    const { fieldId, currentRecord } = context;
-    console.log('fieldChanged', { fieldId, ...context });
-    if (fieldId == 'paymentoption') {
-      const paymentOption = currentRecord.getValue({ fieldId });
-      const isPaymentCard = library.getIsPaymentCard(paymentOption);
-      console.log(`Payment Option (${paymentOption}) is credit card (${isPaymentCard})`);
-      if (isPaymentCard) {
-        // alert(TRANSACTION_FEE_MESSAGE);
-      }
-    }
-  }
 
 
   function saveRecord(context) {
@@ -35,5 +20,5 @@ define(['./commonLibrary'], function (library) {
     }
   }
 
-  return { saveRecord, fieldChanged };
+  return { saveRecord };
 });
